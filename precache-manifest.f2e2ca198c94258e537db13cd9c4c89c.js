b1d87eeaffe499c5864c91630404600e5dac4db9
snerks <martin.github@outlook.com>
2024-04-22 19:23:08 +0100

    Updates

---
+++ precache-manifest.f2e2ca198c94258e537db13cd9c4c89c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d9897cfec61f4c1c3f0334299ee4f9b",
+    "url": "/mui-recommended-gigs-02/index.html"
+  },
+  {
+    "revision": "e30ea20f09782901aa12",
+    "url": "/mui-recommended-gigs-02/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "281fda01e0cc7c55f89a",
+    "url": "/mui-recommended-gigs-02/static/js/2.04365f55.chunk.js"
+  },
+  {
+    "revision": "1ee6b21a3a306b84e6341ef33a0bb834",
+    "url": "/mui-recommended-gigs-02/static/js/2.04365f55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e30ea20f09782901aa12",
+    "url": "/mui-recommended-gigs-02/static/js/main.79902ea0.chunk.js"
+  },
+  {
+    "revision": "eaa5ebf48680bb88bcc8",
+    "url": "/mui-recommended-gigs-02/static/js/runtime-main.de7261af.js"
+  }
+]);
